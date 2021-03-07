5a74cad8198356f58e3d2018af9bc7311725ad9a
Master Jung <ajdkfl6445@gmail.com>
2021-03-07 21:23:23 +0900

    chore: Set Normalize-css styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
+import { NormalizeCss } from "../src/styles/normalize";
+
+export const decorators = [
+  (Story) => (
+    <>
+      <NormalizeCss />
+      <Story />
+    </>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
